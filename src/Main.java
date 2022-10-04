7a7172ead6870cbba4ac72f77db86ff33124ad80
Nekrasov Dmitriy <dimannekr@mail.ru>
2022-10-05 00:23:01 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2500;
+        int oneBonusPrice = 20;
+        int finalBonus = ticketPrice / oneBonusPrice;
+        System.out.println("Вам положено " + finalBonus +  " миль");
+
+    }
+}
